9e3f385e0af6ad1f21ef08b7bb42a2f86ab01d06
小野瑛人 <173251334+eito0518@users.noreply.github.com>
2025-05-16 16:59:26 +0900

    feat(rec): implement basic getRecommendationsHistory logic

--- backend/src/presentation/controller/recommendationController.ts
+++ backend/src/presentation/controller/recommendationController.ts
@@ -1,9 +1,11 @@
 import { GetRecommendationUseCase } from "../../application/usecase/getRecommendationUseCase";
+import { GetRecommendationHistorysUseCase } from "../../application/usecase/getRecommendationHistorysUseCase";
 import { Request, Response } from "express";
 
 export class RecommendationController {
   constructor(
-    private readonly _getRecommendationUseCase: GetRecommendationUseCase
+    private readonly _getRecommendationUseCase: GetRecommendationUseCase,
+    private readonly _getRecommendationHistorysUseCase: GetRecommendationHistorysUseCase
   ) {}
 
   // レコメンドを取得する
@@ -23,5 +25,26 @@ export class RecommendationController {
       })
       .status(200)
       .json({ recommendation: recommendation });
+  }
+
+  // レコメンド履歴を取得する
+  async getHistorys(req: Request, res: Response): Promise<void> {
+    // リクエスト
+    const sessionId = req.cookies.sessionId;
+
+    // ユースケース
+    const historys = await this._getRecommendationHistorysUseCase.run(
+      sessionId
+    );
+
+    // レスポンス
+    res
+      .cookie("sessionId", sessionId, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none", // TODO：　時間があればCSRF対策　で　csurfを導入する
+      })
+      .status(200)
+      .json({ historys: historys });
   }
 }
--- backend/src/presentation/router/recommendationRouter.ts
+++ backend/src/presentation/router/recommendationRouter.ts
@@ -9,3 +9,9 @@ recommendationRouter.get(
   "/api/recommendations",
   asyncHandler(recommendationController.getRecommendations)
 );
+
+// レコメンド履歴を取得するエンドポイント
+recommendationRouter.get(
+  "/api/recommendations/historys",
+  asyncHandler(recommendationController.getHistorys)
+);
