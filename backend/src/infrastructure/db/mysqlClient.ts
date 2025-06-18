cd2f1b1dc32d106ed088ed2cbef5856f16308d4c
小野瑛人 <173251334+eito0518@users.noreply.github.com>
2025-06-18 18:32:45 +0900

    feat(db): add ssl settings for Azure

--- backend/src/infrastructure/db/mysqlClient.ts
+++ backend/src/infrastructure/db/mysqlClient.ts
@@ -1,5 +1,12 @@
 import mysql from "mysql2/promise";
+import fs from "fs";
+import path from "path";
 import { config } from "../../config/config";
+
+const sslCertPath = path.join(
+  __dirname, // 本番環境で実行中のファイルのディレクトリパス (/.../backend/dist/infrastructure/db))
+  "../../../config/cert.prod/DigiCertGlobalRootCA.crt.pem" // そこからの相対パス
+);
 
 export const MysqlClient = mysql.createPool({
   host: config.DB_HOST,
@@ -7,7 +14,12 @@ export const MysqlClient = mysql.createPool({
   password: config.DB_PASSWORD,
   database: config.DB_NAME,
   port: config.DB_PORT,
+  connectionLimit: 10,
   timezone: "+00:00",
   waitForConnections: true,
-  connectionLimit: 10,
+  // NODE_ENVの値によってSSl設定を切り替える
+  ssl:
+    config.NODE_ENV === "production"
+      ? { ca: fs.readFileSync(sslCertPath) }
+      : undefined,
 });
