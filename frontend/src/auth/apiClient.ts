5cf8939a3e5559b7fdb5382fc0ef6f8bc4cae6b9
小野瑛人 <173251334+eito0518@users.noreply.github.com>
2025-06-23 14:13:15 +0900

    fix(frontend): fix apiClient

--- frontend/src/auth/apiClient.ts
+++ frontend/src/auth/apiClient.ts
@@ -1,24 +1,10 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_BASE_URL
+  ? `${import.meta.env.VITE_API_BASE_URL}/api`
+  : "/api";
+
 export const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: baseURL,
   withCredentials: true, // Cookieを許可
 });
-
-// 共通のレスポンスエラーハンドリング
-// apiClient.interceptors.response.use(
-//   (response) => response, // 通信が成功したらそのまま返す
-//   (error) => {
-//     const response = error.response;
-//     // 通信が失敗した時は条件で処理を分岐
-//     if (
-//       response?.status === 401 &&
-//       response?.data?.error_code === "REAUTH_REQUIRED"
-//     ) {
-//       console.warn("Session expired. Redirecting to Login Page");
-//       window.location.href = "/login";
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
