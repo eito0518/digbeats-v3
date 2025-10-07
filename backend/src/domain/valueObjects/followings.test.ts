5907d4621068184030ddd007c85b2f1eff8510fd
小野瑛人 <173251334+eito0518@users.noreply.github.com>
2025-10-07 21:08:13 +0900

    test(recommendation): add unit test for classifyByTrackLikes

--- backend/src/domain/valueObjects/followings.test.ts
+++ backend/src/domain/valueObjects/followings.test.ts
@@ -43,15 +43,68 @@ describe("Followings", () => {
       ];
       const followings = new Followings(artists);
 
-      const expectedMessage =
-        "Insufficient qualified artists. Required: 5, Found: 4";
       const action = () => followings.ensureCanGenerateRecommendation();
 
+      const expectedMessage =
+        "Insufficient qualified artists. Required: 5, Found: 4";
       expect(action).toThrow(RecommendationRequirementsNotMetError);
       expect(action).toThrow(expectedMessage);
     });
   });
 
   // アーティストを いいね曲数 によって分類する
-  describe("", () => {});
+  describe("classifyByTrackLikes", () => {
+    it("should correctly classify artists based on their track likes", () => {
+      // 3つのカテゴリーをすべて含むテストデータを作成
+      const artists = [
+        createMockArtist(200), // standAloneArtists に入る (>= 100)
+        createMockArtist(100), // standAloneArtists に入る (>= 100)
+        createMockArtist(99), // groupableArtists に入る (20 <= x < 100)
+        createMockArtist(20), // groupableArtists に入るはず (20 <= x < 100)
+        createMockArtist(19), // どちらにも入らない (< 20)
+        createMockArtist(0), // どちらにも入らない (< 20)
+      ];
+      const followings = new Followings(artists);
+
+      const { standAloneArtists, groupableArtists } =
+        followings.classifyByTrackLikes();
+
+      // externalUserIdを比較して、中身のアーティストの一致をテスト （ソートすることで確実に一致させる)
+      const standAloneIds = standAloneArtists
+        .map((artist) => artist.externalUserId)
+        .sort();
+      const expectedStandAloneIds = [
+        artists[0].externalUserId,
+        artists[1].externalUserId,
+      ].sort();
+      expect(standAloneIds).toEqual(expectedStandAloneIds);
+
+      const groupableIds = groupableArtists
+        .map((artist) => artist.externalUserId)
+        .sort();
+      const expectedGroupableIds = [
+        artists[2].externalUserId,
+        artists[3].externalUserId,
+      ].sort();
+      expect(groupableIds).toEqual(expectedGroupableIds);
+    });
+
+    it("should return empty arrays when no artists meet the criteria", () => {
+      // すべてのアーティストがいいね20未満のケース
+      const artists = [
+        createMockArtist(19),
+        createMockArtist(15),
+        createMockArtist(10),
+        createMockArtist(5),
+        createMockArtist(0),
+      ];
+      const followings = new Followings(artists);
+
+      const { standAloneArtists, groupableArtists } =
+        followings.classifyByTrackLikes();
+
+      expect(standAloneArtists).toHaveLength(0);
+      expect(groupableArtists).toHaveLength(0);
+    });
+  });
 });
